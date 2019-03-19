3f6aa931684ca3eb9f09ef996177927fa9751029
= <=>
2019-03-19 23:00:50 +0100

    Final copy

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('.fixed-action-btn').floatingActionButton();
+  $('.sidenav').sidenav();
+  $('.materialboxed').materialbox();
+  $('.parallax').parallax();
+  $('.tabs').tabs();
+  $('.datepicker').datepicker(
+    { disableWeekends: true}
+  );
+  $('.tooltipped').tooltip();
+  $('.scrollspy').scrollSpy();
+  $('.carousel').carousel();
+});
